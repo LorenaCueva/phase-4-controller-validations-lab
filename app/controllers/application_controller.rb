e05fdd33b0f56e7cd992f23b8bd04a873ca497e0
Lorena Cueva <lorenacueva@gmail.com>
2023-03-01 12:58:52 -0800

    commit tree b453c60edd97685c27d8c17e4b6ca854348d3c5d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+    private
+
+    def invalid_response invalid
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    end
+
+    def not_found_response
+        render json: {errors: "Record not Found"}, status: :not_found
+    end
 end
